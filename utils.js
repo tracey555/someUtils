99ffd3da98d21fa66fe652448779ae3982df6a6c
Tracey Axelsson <tracey@vcn.bc.ca>
2022-06-02 22:31:40 +0000

    utils.js

---
+++ utils.js
@@ -0,0 +1,11 @@
+function product {num1, num2
+return num1 * num2;
+}
+console.log(product(1, 2));
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+function power(num1, num2) {
+    return Math.pow(num1, num2);
+}
